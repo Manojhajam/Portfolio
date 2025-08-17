12ce0fe34ed2100df58fe0e78e8f67561ba918a2
Manoj Hajam <manojhajam4@gmail.com>
2025-08-17 12:29:16 +0545

    About me

--- my-project/src/components/AboutUs/AboutUs.jsx
+++ my-project/src/components/AboutUs/AboutUs.jsx
@@ -1,11 +1,94 @@
-import React from 'react'
+import React from "react";
+import { ChevronsLeftRight } from "lucide-react";
 
 const AboutUs = () => {
   return (
-    <div className="bg-red-600 h-screen flex items-center justify-center text-4xl">
-      About US
+    <div className="text-4xl  gap-4 bg-slate-50">
+      <div className=" h-screen flex items-center justify-center flex-col gap-8">
+        <h1 className="text-4xl font-bold mb-10">About Me</h1>
+
+        <div className="flex gap-8">
+          <div className="text-lg text-gray-600 flex flex-col gap-6 max-w-2xl mx-auto">
+            <p>
+              Hi, I’m Manoj, a passionate Web Developer with a keen eye for
+              detail and a drive to build impactful digital experiences. I enjoy
+              transforming ideas into clean, efficient, and user-friendly
+              solutions that not only work well but also look great.
+            </p>
+            <p>
+              With a strong foundation in React,
+              Node.js, Tailwind, MongoDB, I love exploring new technologies and
+              continuously improving my craft. Beyond coding, I’m motivated by
+              solving real-world problems, collaborating with creative minds,
+              and delivering projects that make a difference.
+            </p>
+            <p>
+              When I’m not building, you’ll often find me exploring new tech
+              tools, reading about design trends, or enjoying
+              music/fitness/travel.
+            </p>
+          </div>
+
+          <div className="flex gap-6 max-w-2xl mx-auto">
+            <div className="flex flex-col gap-5">
+              <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
+                <div className="">
+                  <ChevronsLeftRight color="black" />
+                </div>
+                <h4 className="text-lg font-semibold text-gray-900 mb-2">
+                  Clean Code
+                </h4>
+                <p className="text-sm text-gray-600">
+                  Writing maintainable, scalable, and efficient code following
+                  best practices.
+                </p>
+              </div>
+
+              <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
+                <div className="">
+                  <ChevronsLeftRight color="black" />
+                </div>
+                <h4 className="text-lg font-semibold text-gray-900 mb-2">
+                  Clean Code
+                </h4>
+                <p className="text-sm text-gray-600">
+                  Writing maintainable, scalable, and efficient code following
+                  best practices.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-5">
+              <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
+                <div className="">
+                  <ChevronsLeftRight color="black" />
+                </div>
+                <h4 className="text-lg font-semibold text-gray-900 mb-2">
+                  Clean Code
+                </h4>
+                <p className="text-sm text-gray-600">
+                  Writing maintainable, scalable, and efficient code following
+                  best practices.
+                </p>
+              </div>
+              <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
+                <div className="">
+                  <ChevronsLeftRight color="black" />
+                </div>
+                <h4 className="text-lg font-semibold text-gray-900 mb-2">
+                  Clean Code
+                </h4>
+                <p className="text-sm text-gray-600">
+                  Writing maintainable, scalable, and efficient code following
+                  best practices.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default AboutUs
+export default AboutUs;
--- my-project/src/components/Home/Home.jsx
+++ my-project/src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import manoj from "../../assets/manoj.jpg"
 
 const Home = () => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center text-4xl">
+    <div className="h-screen flex flex-col justify-center items-center text-4xl gap-5">
       <div className="photo">
         <img
           className="w-[250px] h-[250px] rounded-full border-4 border-blue-600"
@@ -16,13 +16,23 @@ const Home = () => {
         <h1 className="text-5xl font-bold mt-5">
           Hi, I'm <span className="text-blue-500">Manoj Hajam</span>
         </h1>
-        <h2 className='font-semibold'>Software Developer</h2>
-        <p className="text-xl">
+        <h2 className="text-2xl md:text-3xl text-muted-foreground text-center">
+          Software Developer
+        </h2>
+        <p className="text-xl text-center">
           {" "}
           I create stunning web app and mobile app experiences with clean code
           and <br />
           modern design. Let's build something amazing together.
         </p>
+      </div>
+      <div className="flex gap-4">
+        <button className="bg-blue-600 text-white px-7 py-2 rounded-lg font-semibold hover:bg-blue-700 transition-colors shadow-lg text-lg">  
+          Contact Me
+        </button>
+        <button className="border-2 border-blue-400 text-blue-400 px-8 py-3 rounded-lg font-semibold hover:bg-blue-400 hover:text-gray-900 transition-colors shadow-lg text-lg">
+          Learn More
+        </button>
       </div>
     </div>
   );
