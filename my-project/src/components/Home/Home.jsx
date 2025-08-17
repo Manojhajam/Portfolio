15617d25768192ca539a00256e98740cc681bacc
Manoj Hajam <manojhajam4@gmail.com>
2025-08-17 08:16:18 +0545

    homepage added something

--- my-project/src/components/Home/Home.jsx
+++ my-project/src/components/Home/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import manoj from "../../assets/manoj.jpg"
+
 
 const Home = () => {
   return (
-    <div className="h-screen bg-cyan-200 flex items-center justify-center text-4xl">
-      Home
+    <div className="h-screen flex flex-col justify-center items-center text-4xl">
+      <div className="photo">
+        <img
+          className="w-[250px] h-[250px] rounded-full border-4 border-blue-600"
+          src={manoj}
+          alt="Manoj"
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center gap-3">
+        <h1 className="text-5xl font-bold mt-5">
+          Hi, I'm <span className="text-blue-500">Manoj Hajam</span>
+        </h1>
+        <h2 className='font-semibold'>Software Developer</h2>
+        <p className="text-xl">
+          {" "}
+          I create stunning web app and mobile app experiences with clean code
+          and <br />
+          modern design. Let's build something amazing together.
+        </p>
+      </div>
     </div>
   );
 }
