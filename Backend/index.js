a43fa57b8610030f6e6d62bc0accb4b3cf211584
Manoj Hajam <manojhajam4@gmail.com>
2025-08-24 15:50:35 +0545

    Backed & connected to db

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const PORT = 5000;
+const app = express();
+
+app.get("/test", (req, res) => {
+   res.send("Hello world")    
+})
+
+const connectToDB = async () => {
+    try {
+        const response = await mongoose.connect("mongodb://localhost:27017/Portfolio");
+        console.log("Connected to DB", response.connection.name);
+    } catch (error) {
+        console.log(error)
+    }
+    
+} 
+connectToDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT ${PORT}`)
+})
+
