5593a31a725571081fd2b29f37a4a2f4b840e8a4
Manoj Hajam <manojhajam4@gmail.com>
2025-08-26 07:25:57 +0545

    remove backend

--- Backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { contactControllers } from "../controllers/contactControllers.js";
-
-const contactRouter = express.Router();
-
-contactRouter.route("/").post(contactControllers);
-
-export default contactRouter;
--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
       <Element name="contact">
         <Contact />
       </Element>
-      <Footer/>
+      <Footer />
     </>
   );
 };
