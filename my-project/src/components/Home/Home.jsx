183ad3cf1776f164d6c4769bd37634f9d69d0a48
manojhajam <manojhajam4@gmail.com>
2026-04-28 10:35:09 +0545

    width maintain in home

--- my-project/src/components/Home/Home.jsx
+++ my-project/src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-scroll";
 const Home = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center text-4xl gap-5">
-      <div className="w-[250px] h-[250px] rounded-full border-4 border-blue-600 overflow-hidden">
+      <div className="w-[250px] h-[250px] rounded-full border-4 border-blue-600 overflow-hidden max-sm:mt-20" >
         <img
           className="w-full h-full object-cover"
           src="/assets/pp.jpg"
